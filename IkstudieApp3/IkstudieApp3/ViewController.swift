71cb54e1e30df87959357dfc9777e188438f90bb
satoru0628 <satoru.ohnishi0628@gmail.com>
2017-07-30 18:35:07 +0900

    Initial Commit

---
+++ IkstudieApp3/IkstudieApp3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IkstudieApp3
+//
+//  Created by 大西智 on 2017/07/30.
+//  Copyright © 2017年 ikstudie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
