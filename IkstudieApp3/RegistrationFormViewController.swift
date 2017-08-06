69f11c4e97344d2820798d05dd636325afe44258
satoru0628 <satoru.ohnishi0628@gmail.com>
2017-08-06 23:18:37 +0900

    0806作業分

---
+++ IkstudieApp3/RegistrationFormViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RegistrationFormViewController.swift
+//  IkstudieApp3
+//
+//  Created by 大西智 on 2017/08/06.
+//  Copyright © 2017年 ikstudie. All rights reserved.
+//
+
+import UIKit
+
+class RegistrationFormViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
